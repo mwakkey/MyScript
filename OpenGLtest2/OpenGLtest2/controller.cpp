b4441796da16d3548019418aaa81b6de8bcb03bd
miyawaki masaya <oingoboingo3911@outlook.com>
2016-06-27 15:50:33 +0900

    update

--- OpenGLtest2/OpenGLtest2/controller.cpp
+++ OpenGLtest2/OpenGLtest2/controller.cpp
@@ -1,47 +1,43 @@
 #include "controller.h"
 #include <vector>
 #include <gl/glut.h>
-
+#include <numeric>
 
 //ゲーム内でのキー番号と対応するキーのマップ
-std::vector<bool> keyFlag;
+unsigned int keyFlag;
 
-void Controller::setController(int buttons)
+void Controller::setController()
 {
-	keyFlag.resize(buttons);
+	flagClear();
 }
 
 void Controller::joystick(unsigned int ButtonMask, int x, int y, int z)
 {
-	flagClear();
+	unsigned int tmp = 0;
+	if (x > 300)          tmp += 1;//right
+	if (x < -300)         tmp += 2;//left
+	if (y > 300)          tmp += 4;//up
+	if (y < -300)         tmp += 8;//down
 
-	if (x > 300)          keyFlag[0] = true;//right
-	if (x < -300)         keyFlag[1] = true;//left
-	if (y > 300)          keyFlag[2] = true;//up
-	if (y < -300)         keyFlag[3] = true;//down
-
-	if (ButtonMask & 1)   keyFlag[4] = true;//A
-	if (ButtonMask & 2)   keyFlag[5] = true;//B
-	if (ButtonMask & 4)   keyFlag[6] = true;//C
-	if (ButtonMask & 8)   keyFlag[7] = true;//X
-	if (ButtonMask & 16)  keyFlag[8] = true;//Y
-	if (ButtonMask & 32)  keyFlag[9] = true;//Z
-	if (ButtonMask & 64)  keyFlag[10] = true;//L
-	if (ButtonMask & 128) keyFlag[11] = true;//R
-	if (ButtonMask & 256) keyFlag[12] = true;//Start
+	keyFlag = (ButtonMask << 4) + tmp;
 
 }
 
 void Controller::flagClear()
 {
-	for each(bool flag in keyFlag)
-	{
-		flag = false;
-	}
-
+	keyFlag = 0;
 }
 
 void Controller::inputJoyStick(int Hz)
 {
 	glutJoystickFunc(joystick, Hz);//Hz[ms]に一回ジョイスティック入力を受け付ける
 }
+
+
+bool Controller::putDown(unsigned int buttonID)
+{
+
+	return (keyFlag & (2 << buttonID));
+
+}
+
--- OpenGLtest2/OpenGLtest2/controller.h
+++ OpenGLtest2/OpenGLtest2/controller.h
@@ -2,12 +2,11 @@
 
 namespace Controller
 {
-	void setController(int buttons);
-
+	void setController();
 	void flagClear();
 
 	void joystick(unsigned int ButtonMask, int x, int y, int z);
 	void inputJoyStick(int Hz);
 	
-
+	bool putDown(unsigned int butonID);
 };
