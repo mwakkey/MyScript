42b3d07979774c6086a5040e0243b3b598716633
miyawaki masaya <oingoboingo3911@outlook.com>
2016-07-11 15:30:10 +0900

    a

--- OpenGLtest2/OpenGLtest2/convolution.h
+++ OpenGLtest2/OpenGLtest2/convolution.h
@@ -20,11 +20,11 @@ namespace MathFunc {
 
 		//離散関数(スペクトル)の畳み込み(0<=i<=right)
 		template <typename T>
-		static T conv(const std::vector<T>& f, const std::vector<T>& g) {
+		static T conv(const std::vector<T>& f, const std::vector<T>& g,int t) {
 			T tmp = (T)0;
 			//範囲外の値を参照しても増分は0なので計算しない
 			int right;
-			right = std::min(f.size(), g.size()) - 1;
+			right = (std::min(f.size(), g.size()) - 1) > t ? t : (std::min(f.size(), g.size()) - 1);
 
 			for (int i = 0; i <= right; ++i) {
 				tmp += f.at(i) *g.at(right-i);
