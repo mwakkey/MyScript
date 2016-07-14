cd970f5afbba4b910c3e6b366309b73cfc5eb53c
miyawaki masaya <oingoboingo3911@outlook.com>
2016-07-14 10:24:08 +0900

    derivative

---
+++ OpenGLtest2/OpenGLtest2/derivative.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <functional>
+
+
+class Derivative
+{
+public:
+	template <typename T>
+	static T derivative(std::function<T(T x)>& f, T coef, float delta)
+	{
+		return (f(coef + delta) - f(coef)) / delta;
+	}
+
+	template <typename T>
+	static T derivative(std::function<T(T x, T y)>& f, T coef1,T coef2, float delta,int n)
+	{
+		if (n == 1) {
+			return (f(coef1 + delta, coef2) - f(coef1, coef2)) / delta;
+		}
+		if (n == 2) {
+			return (f(coef1, coef2+delta) - f(coef1, coef2)) / delta;
+		}
+	}
+};
