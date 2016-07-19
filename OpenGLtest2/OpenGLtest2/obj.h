0e2df5da5c70b30cf0027f45dbf41fed853dccd5
miyawaki masaya <oingoboingo3911@outlook.com>
2016-07-19 17:01:52 +0900

    model

--- OpenGLtest2/OpenGLtest2/obj.h
+++ OpenGLtest2/OpenGLtest2/obj.h
@@ -3,84 +3,56 @@
 #include <vector>
 #include <string>
 
-//3つのベクトル
-struct Vector3f
+#define DEF_BUF_SIZE 2
+
+//モデルデータを表すクラス
+class Model
 {
-	float x;
-	float y;
-	float z;
-}vec3d;
-//4つのベクトル
-struct Vector4f
-{
-	float x;
-	float y;
-	float z;
-	float w;
-}vec4d;
-//4つの反射
-struct Reflection4
-{
-	float diffuse;
-	float ambient;
-	float emission;
-	float specular;
-};
-//4つのカラー
-struct Color4
-{
-	float r;
-	float g;
-	float b;
-	float a;
-};
-//UV座標
-struct UV
-{
-	float u;//u値
-	float v;//v値
-}vec2d;
-struct Vector4I
-{
-	int x;
-	int y;
-	int z;
-	int w;
-};
-//ポリゴンデータ
-struct Triangle
-{
-	Vector3f TriVer;
-	Vector3f TriNor;
-	UV TriUV;
-}Tri;
-//ポリゴンデータ
-struct Quadrangle
-{
-	Vector3f QuadVer;
-	Vector3f QuadNor;
-	UV QuadUV;
-}Quad;
-//マテリアル構造体
-struct MATERIAL
-{
-	int MaterialID;//ID
-	std::string MaterialName;//マテリアル名
-	Color4 Color;//カラー
-	Reflection4 ReflectionColor;//反射
-	float Power;//shiness
-	std::string TextureName;//テクスチャ名
-}mtl;
-//モデルクラス
-class MODEL
-{
-protected:
-	bool LoadMaterialFromFile(char* FileName);//マテリアルロード
-	std::vector <Triangle> Tridata;
-	std::vector <Quadrangle> Quaddata;
+private:
+	std::vector<float> vertices;
+	int numVertices;
+
 public:
-	MODEL();
-	MODEL(char* FileName);//コンストラクタ
-	bool OBJ_Load(char* FileName);//ロード
-	void Draw();
+
+};
+
+
+//OBJファイルからモデルデータを読み込むクラス
+class Obj
+{
+private:
+	TBuffer<float> *vs;
+	TBuffer<int> *fs;
+
+public:
+	Obj();
+	~Obj();
+	Model* loadObj(const std::string fileName);
+	void readVertices(const std::string line);
+	void readIndices(const std::string line);
+	Model* createModel();
+};
+
+//モデルデータ読み込みのためのバッファクラス
+template <typename T>
+class TBuffer
+{
+private:
+	int bufSize;
+	std::vector<T> buf;
+public:
+	TBuffer()
+	{
+		bufSize = DEF_BUF_SIZE;
+		buf.resize(bufSize);
+	}
+	void add(T value)
+	{
+		buf.push_back(value);
+	}
+
+	T get(int index)
+	{
+		return buf.at(index);
+	}
 };
